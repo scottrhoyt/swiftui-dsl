209315a69016093a5828a58894ac682cb0c80be2
Scott Hoyt <scottrhoyt@gmail.com>
2025-07-29 21:44:22 -0700

    Update to alternative MLN build.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution.git", from: "6.10.0"),
+        .package(url: "https://github.com/scottrhoyt/maplibre-gl-native-distribution.git", from: "6.18.17"),
         // Macros
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0" ..< "602.0.0"),
         // Testing
